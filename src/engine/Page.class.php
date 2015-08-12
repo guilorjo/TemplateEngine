eedeb3f999d9181f1b8055bdd6e195b1ec9bffb9
Guillaume Marques <guillaume.marques33@gmail.com>
2015-08-12 13:16:07 +0200

    ajout balises meta

--- src/engine/Page.class.php
+++ src/engine/Page.class.php
@@ -12,32 +12,37 @@
 class Page {
 
 	public static function defineMetaElements($meta){
-
+		$str = '';
+		foreach($meta as $name => $content){
+			if($name == 'title'){
+				$str .= '<title>'.$content.'</title>';
+			} else {
+				$str .= '<meta name="'.$name.'" content="'.$content.'"/>';
+			}
+		}
+		return $str;
 	}
 
 	public static function addJavascriptDocuments($js){
+		$str = '';
 		foreach($js as $key => $value){
-			echo '<script type="text/javascript" src="'.htmlentities($value).'"></script>';
+			$str.= '<script type="text/javascript" src="'.htmlentities($value).'"></script>';
 		}
+		return $str;
 	}
 
 	public static function addCSSDocuments($css){
+		$str = '';
 		foreach($css as $key => $value){
-			echo '<link rel="stylesheet" type="text/css" href="'.htmlentities($css).'" media="screen"/>';
+			$str .= '<link rel="stylesheet" type="text/css" href="'.htmlentities($css).'" media="screen"/>';
 		}
+		return $str;
 	}
 
 	public static function minification($html){
 			//Minification
-	    $search = array(
-	        '/ {2,}/',
-	        '/<!--.*?-->|\t|(?:\r?\n[ \t]*)+/s'
-	    );
-
-	    $replace =  array(
-	        ' ',
-	        ''
-	    );
+	    $search = array( '/ {2,}/', '/<!--.*?-->|\t|(?:\r?\n[ \t]*)+/s');
+	    $replace =  array(' ', '');
 	    return preg_replace($search, $replace, $html);
 	}
 }
